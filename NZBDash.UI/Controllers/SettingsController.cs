b004e82ea14ac2401040c05cd44ca72bfe8db6a4
tidusjar <jamierees@outlook.com>
2015-10-02 12:08:38 +0100

    Started Settings page

---
+++ NZBDash.UI/Controllers/SettingsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NZBDash.UI.Controllers
+{
+    public class SettingsController : Controller
+    {
+        // GET: Settings
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateNzbGetSettings()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateSabNzbSettings()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateSonarrSettings()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateCouchPotatoSettings()
+        {
+            return View();
+        }
+
+        public ActionResult UpdatePlexSettings()
+        {
+            return View();
+        }
+    }
+}
