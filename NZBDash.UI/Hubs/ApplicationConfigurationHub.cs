8e022331ff1b2a9174c270f36e8d7b2dc4768c7b
tidusjar <jamierees@outlook.com>
2015-10-06 13:18:49 +0100

    - Changed hub so we are testing individual applications now - Changed the config.js so we are testing individual apps - Added to the NZBGet settings page

--- NZBDash.UI/Hubs/ApplicationConfigurationHub.cs
+++ NZBDash.UI/Hubs/ApplicationConfigurationHub.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Web.Script.Serialization;
 
 using Microsoft.AspNet.SignalR;
 
 using NZBDash.Common;
-using NZBDash.Core.Configuration;
-using NZBDash.Core.Model.DTO;
 using NZBDash.UI.Helpers;
-using NZBDash.UI.Models;
 
 namespace NZBDash.UI.Hubs
 {
@@ -15,20 +11,10 @@ namespace NZBDash.UI.Hubs
     {
         public void TestNzbGetConnection(string ipAddress, int port, string username, string password)
         {
-            var selectedApp = Applications.NzbGet;
+            const Applications selectedApp = Applications.NzbGet;
+
             var tester = new EndpointTester();
             var uri = UrlHelper.ReturnUri(ipAddress, port);
-
-
-        }
-
-        public void TestConnection(int application, string apiKey, string ipAddress, string password, string userName)
-        {
-            var selectedApp = (Applications)application;
-
-            var tester = new EndpointTester(); // Need to work out how to identify the tester type
-
-            var uri = UrlHelper.ReturnUri(ipAddress);
             if (uri == null)
             {
                 Clients.All.failed(string.Format("Incorrect IP Address"));
@@ -37,8 +23,7 @@ namespace NZBDash.UI.Hubs
 
             try
             {
-                var result = tester.TestApplicationConnectivity((Applications)application, apiKey, uri.ToString(), password, userName);
-
+                var result = tester.TestApplicationConnectivity(selectedApp, string.Empty, uri.ToString(), password, username);
                 if (!result)
                 {
                     Clients.All.failed(string.Format("Could not connect to {0}", selectedApp));
@@ -51,49 +36,6 @@ namespace NZBDash.UI.Hubs
             {
                 Clients.All.failed(string.Format("{0}", e.Message));
             }
-        }
-
-        // This is not used.
-        public void AddApplication(string applicationJson)
-        {
-            var model = new JavaScriptSerializer().Deserialize<ApplicationConfigurationViewModel>(applicationJson);
-
-            var admin = new AdminConfiguration();
-            var dto = MapToDto(model);
-            var result = admin.UpdateApplicationConfiguration(dto);
-            if (result)
-            {
-                Clients.All.message(string.Format("Successfully updated {0}", model.ApplicationName));
-            }
-            Clients.All.failed(string.Format("Could not update {0}", model.ApplicationName));
-        }
-
-        private static ApplicationConfigurationDto MapToDto(ApplicationConfigurationViewModel model)
-        {
-            var dto = new ApplicationConfigurationDto
-            {
-                Id = model.Id,
-                Password = model.Password,
-                Username = model.Username,
-                ApiKey = model.ApiKey,
-                IpAddress = model.IpAddress,
-                ApplicationName = model.ApplicationName,
-                ApplicationId = model.ApplicationId
-            };
-
-            return dto;
-        }
-
-        private bool Validate(string ipAddress)
-        {
-            var errors = false;
-            if (string.IsNullOrEmpty(ipAddress))
-            {
-                Clients.All.failed("Please enter in an IP Address");
-                errors = true;
-            }
-
-            return !errors;
         }
     }
 }
