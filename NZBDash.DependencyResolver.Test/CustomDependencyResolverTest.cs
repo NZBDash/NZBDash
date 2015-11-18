5dd78f1ba68673d5b6e14aeef5661283ab16d477
tidusjar <jamierees@outlook.com>
2015-11-18 11:45:59 +0000

    Added missing test project

---
+++ NZBDash.DependencyResolver.Test/CustomDependencyResolverTest.cs
@@ -0,0 +1,54 @@
+﻿using Ninject;
+
+using NUnit.Framework;
+
+using NZBDash.Common.Interfaces;
+using NZBDash.Core.Interfaces;
+using NZBDash.Core.Model.Settings;
+using NZBDash.DependencyResolver.Modules;
+
+namespace NZBDash.DependencyResolver.Test
+{
+    [TestFixture]
+    public class CustomDependencyResolverTest
+    {
+        [Test]
+        public void ResolveServiceDependency()
+        {
+            var module = new ServiceModule();
+            var kernal = new StandardKernel(module);
+
+            var service = kernal.Get<IHardwareService>();
+            Assert.That(service,Is.Not.Null);
+        }
+
+        [Test]
+        public void ResolveApplicationDependency()
+        {
+            var module = new ApplicationSettingsModule();
+            var kernal = new StandardKernel(module);
+
+            var service = kernal.Get<ISettingsService<PlexSettingsDto>>();
+            Assert.That(service, Is.Not.Null);
+        }
+
+        [Test]
+        public void ResolveSerializerDependency()
+        {
+            var module = new SerializerModule();
+            var kernal = new StandardKernel(module);
+
+            var service = kernal.Get<ISerializer>();
+            Assert.That(service, Is.Not.Null);
+        }
+
+        [Test]
+        public void GetCustomModules()
+        {
+            var customResolver = new CustomDependencyResolver();
+            var modules = customResolver.GetModules();
+
+            Assert.That(modules.Count, Is.GreaterThan(0));
+        }
+    }
+}
