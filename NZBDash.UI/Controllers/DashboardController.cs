197a746bc2f938a4347fdc0dd91b11bbe2c54d59
Jamie Rees <jamie.rees@exclaimer.com>
2016-01-17 13:50:20 +0000

    Added a currently downloading dropdown to the navbar

--- NZBDash.UI/Controllers/DashboardController.cs
+++ NZBDash.UI/Controllers/DashboardController.cs
@@ -1,13 +1,14 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
-
+using NZBDash.Common.Helpers;
 using NZBDash.Common.Interfaces;
 using NZBDash.Common.Mapping;
 using NZBDash.Core.Interfaces;
 using NZBDash.Core.Model.Settings;
 using NZBDash.ThirdParty.Api.Interfaces;
 using NZBDash.UI.Helpers;
+using NZBDash.UI.Models;
 using NZBDash.UI.Models.Dashboard;
 using NZBDash.UI.Models.Hardware;
 using NZBDash.UI.Models.ViewModels.Dashboard;
@@ -117,6 +118,83 @@ namespace NZBDash.UI.Controllers
                 Logger.Trace("No settings found. Cannot display downloads on the Dashboard");
             }
             return PartialView("Partial/_Download", model);
+        }
+
+        public ActionResult GetTabDownloads()
+        {
+            var model = new TabDownloadViewModel();
+            var sab = Sab.GetSettings();
+            var nzb = NzbGet.GetSettings();
+            if (sab.HasSettings)
+            {
+                var formattedUri = Common.Helpers.UrlHelper.ReturnUri(sab.IpAddress, sab.Port).ToString();
+                var items = Api.GetSabNzbdQueue(formattedUri, sab.ApiKey);
+
+                model.DownloadSpeed = MemorySizeConverter.SizeSuffix((long) items.kbpersec);
+                model.Application = "Sabnzbd";
+                foreach (var dl in items.jobs)
+                {
+                    var percentage = (dl.mbleft / dl.mb * 100);
+                    Logger.Trace(string.Format("Percentage : {0}", percentage));
+
+                    var status = EnumHelper<DownloadStatus>.Parse(items.paused ? "PAUSED" : "DOWNLOADING");
+                    var progressBar = Bootstrap.ProgressBarDanger;
+                    if (status == DownloadStatus.PAUSED || status == DownloadStatus.QUEUED)
+                    {
+                        progressBar = Bootstrap.ProgressBarWarning;
+                    }
+                    if (status == DownloadStatus.DOWNLOADING)
+                    {
+                        progressBar = Bootstrap.ProgressBarSuccess;
+                    }
+                    model.Downloads.Add(new TabDownloadItems
+                    {
+                        DownloadName = dl.filename,
+                        Status = status.ToString(),
+                        DownloadPercentage = percentage,
+                        ProgressBarClass = progressBar
+                    });
+                }
+               
+            }
+            else if (nzb.HasSettings)
+            {
+                var formattedUri = Common.Helpers.UrlHelper.ReturnUri(nzb.IpAddress, nzb.Port).ToString();
+                var statusInfo = Api.GetNzbGetStatus(formattedUri, nzb.Username, nzb.Password);
+                var nzbItem = Api.GetNzbGetList(formattedUri, nzb.Username, nzb.Password);
+
+                model.DownloadSpeed = MemorySizeConverter.SizeSuffix(statusInfo.Result.DownloadRate / 1024);
+                model.Application = "NzbGet";
+
+                foreach (var dl in nzbItem.result)
+                {
+                    var percentage = (dl.DownloadedSizeMB / (dl.RemainingSizeMB + (double)dl.DownloadedSizeMB) * 100);
+                    Logger.Trace(string.Format("Percentage : {0}", percentage));
+
+                    var status = EnumHelper<DownloadStatus>.Parse(dl.Status);
+                    var progressBar = "progress-bar-danger";
+                    if (status == DownloadStatus.PAUSED || status == DownloadStatus.QUEUED)
+                    {
+                        progressBar = "progress-bar-warning";
+                    }
+                    if (status == DownloadStatus.DOWNLOADING)
+                    {
+                        progressBar = "progress-bar-success";
+                    }
+                    model.Downloads.Add(new TabDownloadItems
+                    {
+                        DownloadName = dl.NZBName,
+                        Status = status.ToString(),
+                        DownloadPercentage = percentage,
+                        ProgressBarClass = progressBar
+                    });
+                }
+            }
+            else
+            {
+                Logger.Trace("No settings found. Cannot display downloads on the Dashboard");
+            }
+            return PartialView("NavbarDownloads", model);
         }
     }
 }
